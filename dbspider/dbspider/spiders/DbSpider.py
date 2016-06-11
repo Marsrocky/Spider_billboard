3fbfb08cb82d71bf566a19906976401300a29a62
杨剑飞 <jianfei@yangjianfeideMacBook-Pro.local>
2016-06-11 17:14:55 +0800

    complete single movie spider

--- dbspider/dbspider/spiders/DbSpider.py
+++ dbspider/dbspider/spiders/DbSpider.py
@@ -24,8 +24,26 @@ class DbSpider(scrapy.spiders.Spider):
 		# Approval of comment
 		item['approval'] = sel.xpath('//div[@class="comment"]/h3/span[@class="comment-vote"]/span[contains(@class, "votes")]/text()[1]').extract()
 		# Comment of movie
-		item['comment'] = sel.xpath('//div[@class="comment"]/p[@class=""]/text()[1]')
+		item['comment'] = sel.xpath('//div[@class="comment"]/p[@class=""]/text()[1]').extract()
+
+		with open('data_result.txt', 'a') as f:
+			for i in range(20):
+				# f.write('num: %s\n' % str(i+1))
+				f.write('time: %s\n' % item['time'][i].strip().encode('utf-8'))
+				f.write('approval: %s\n' % item['approval'][i].encode('utf-8'))
+				f.write('comment: %s\n' % item['comment'][i].strip().encode('utf-8'))
+
+				# Due to the possible lost data, we don`t use this data
+				# f.write('grade: %s\n' % item['grade'][i].encode('utf-8'))		
+
+				f.write('\n')
+			f.close()
+
 		yield item
 
-		# with open('text', 'wb') as f:
-		# 	f.write(response.body)
+		next_page = '//div[@id="paginator"]/a[@class="next"]/@href'
+		if response.xpath(next_page):
+			next_url = 'https://movie.douban.com/subject/20438962/comments' + response.xpath(next_page).extract()[0]
+			request = scrapy.Request(next_url, callback = self.parse)
+			yield request
+
